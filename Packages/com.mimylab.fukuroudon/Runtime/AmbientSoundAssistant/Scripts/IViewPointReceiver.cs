ebed134480ee564f888bc648e03023f7a0608ca2
MimyQuality <mimyquality@gmail.com>
2024-03-21 21:12:05 +0900

    AmbientSoundAssistant実装

---
+++ Packages/com.mimylab.fukuroudon/Runtime/AmbientSoundAssistant/Scripts/IViewPointReceiver.cs
@@ -0,0 +1,18 @@
+﻿/*
+Copyright (c) 2024 Mimy Quality
+Released under the MIT license
+https://opensource.org/licenses/mit-license.php
+*/
+
+namespace MimyLab
+{
+    using UdonSharp;
+    using UnityEngine;
+    //using VRC.SDKBase;
+    //using VRC.Udon;
+
+    public class IViewPointReceiver : UdonSharpBehaviour
+    {
+        public virtual void ReceiveViewPoint(Vector3 position, Quaternion rotation) { }
+    }
+}
