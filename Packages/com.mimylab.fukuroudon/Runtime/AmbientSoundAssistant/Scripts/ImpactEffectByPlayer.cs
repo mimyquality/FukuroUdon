c50bf30dd0d1e823032341ff82f8f75503e60213
MimyQuality <mimyquality@gmail.com>
2024-04-02 22:07:50 +0900

    ImpactEffectByPlayer追加

---
+++ Packages/com.mimylab.fukuroudon/Runtime/AmbientSoundAssistant/Scripts/ImpactEffectByPlayer.cs
@@ -0,0 +1,27 @@
+﻿/*
+Copyright (c) 2024 Mimy Quality
+Released under the MIT license
+https://opensource.org/licenses/mit-license.php
+*/
+
+namespace MimyLab
+{
+    using UdonSharp;
+    using UnityEngine;
+    using VRC.SDKBase;
+    //using VRC.Udon;
+    //using VRC.SDK3.Components;
+
+    [AddComponentMenu("Fukuro Udon/Ambient Sound Assistant/Impact Effect by Player")]
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class ImpactEffectByPlayer : ImpactEffect
+    {
+        public override void OnPlayerTriggerEnter(VRCPlayerApi player)
+        {
+            Debug.Log("On Impact Player");
+            if (!Utilities.IsValid(player)) { return; }
+
+            PlayEffect(player.GetPosition(), player.GetVelocity());
+        }
+    }
+}
