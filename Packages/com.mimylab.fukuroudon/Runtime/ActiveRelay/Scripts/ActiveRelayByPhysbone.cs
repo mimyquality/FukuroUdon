48f0aff5023e6a91f2806c8d0e82e03c11f777f1
mimyquality <mimyquality@gmail.com>
2025-10-27 19:10:28 +0900

    VRCSDK 3.9.1-beta.1 反映

---
+++ Packages/com.mimylab.fukuroudon/Runtime/ActiveRelay/Scripts/ActiveRelayByPhysbone.cs
@@ -0,0 +1,36 @@
+﻿/*
+Copyright (c) 2025 Mimy Quality
+Released under the MIT license
+https://opensource.org/licenses/mit-license.php
+*/
+
+namespace MimyLab.FukuroUdon
+{
+    using UdonSharp;
+    using UnityEngine;
+    using VRC.SDKBase;
+    using VRC.Udon;
+    using VRC.SDK3.Components;
+
+    [UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+    public class ActiveRelayByPhysbone : UdonSharpBehaviour
+    {
+
+
+        private bool _initialized = false;
+        private void Initialize()
+        {
+            if (_initialized) { return; }
+
+
+
+            _initialized = true;
+        }
+        private void Start()
+        {
+            Initialize();
+
+
+        }
+    }
+}
