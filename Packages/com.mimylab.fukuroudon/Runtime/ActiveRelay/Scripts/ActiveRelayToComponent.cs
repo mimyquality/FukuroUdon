8a1ae92ad5ae1325d2c8a91565cb8ad2cecb39d9
mimyquality <mimyquality@gmail.com>
2025-10-29 01:10:21 +0900

    to Component 見直し

--- Packages/com.mimylab.fukuroudon/Runtime/ActiveRelay/Scripts/ActiveRelayToComponent.cs
+++ Packages/com.mimylab.fukuroudon/Runtime/ActiveRelay/Scripts/ActiveRelayToComponent.cs
@@ -9,6 +9,11 @@ namespace MimyLab.FukuroUdon
     using UdonSharp;
     using UnityEngine;
     using UnityEngine.Animations;
+    using VRC.Dynamics;
+    using VRC.SDK3.Dynamics.Constraint.Components;
+    using VRC.SDK3.Dynamics.Contact.Components;
+    using VRC.SDK3.Dynamics.PhysBone.Components;
+    using VRC.Udon;
 
     [HelpURL("https://github.com/mimyquality/FukuroUdon/wiki/Active-Relay#activerelay-to-component")]
     [Icon(ComponentIconPath.FukuroUdon)]
@@ -43,23 +48,40 @@ namespace MimyLab.FukuroUdon
         {
             if (!component) { return false; }
 
-            if (component is Collider)
-            {
-                if (component.GetType() == typeof(TerrainCollider)) { return false; }
-                return true;
-            }
-            if (component is Renderer)
-            {
-                if (component.GetType() == typeof(UnityEngine.Tilemaps.TilemapRenderer)) { return false; }
-                return true;
-            }
-            if (component is Behaviour)
-            {
-                return true;
-            }
-            // Extra
+            if (component is Collider) { return true; }
+            if (component is Renderer) { return true; }
+            // 個別に羅列の必要ある
+            //if (component is Behaviour) { return true; }
             if (component is OcclusionPortal) { return true; }
             if (component is CanvasGroup) { return true; }
+            if (component is ParentConstraint) { return true; }
+            if (component is PositionConstraint) { return true; }
+            if (component is RotationConstraint) { return true; }
+            if (component is ScaleConstraint) { return true; }
+            if (component is AimConstraint) { return true; }
+            if (component is LookAtConstraint) { return true; }
+            if (component is AudioSource) { return true; }
+            if (component is AudioLowPassFilter) { return true; }
+            if (component is AudioHighPassFilter) { return true; }
+            if (component is AudioEchoFilter) { return true; }
+            if (component is AudioDistortionFilter) { return true; }
+            if (component is AudioChorusFilter) { return true; }
+            if (component is AudioReverbFilter) { return true; }
+            if (component is AudioReverbZone) { return true; }
+            if (component is Light) { return true; }
+            if (component is Camera) { return true; }
+            if (component is Animator) { return true; }
+            if (component is UdonBehaviour) { return true; }
+            //if (component is UdonSharpBehaviour) { return true; } // なんとかする
+            if (component is VRCParentConstraint) { return true; }
+            if (component is VRCPositionConstraint) { return true; }
+            if (component is VRCRotationConstraint) { return true; }
+            if (component is VRCScaleConstraint) { return true; }
+            if (component is VRCAimConstraint) { return true; }
+            if (component is VRCLookAtConstraint) { return true; }
+            if (component is VRCPhysBone) { return true; }
+            if (component is VRCContactSender) { return true; }
+            if (component is VRCContactReceiver) { return true; }
 
             return false;
         }
@@ -90,37 +112,49 @@ namespace MimyLab.FukuroUdon
                 if (!component) continue;
 
                 var type = component.GetType();
-                if (type == typeof(GameObject)) { return; }
-                // Collider
-                else if (type == typeof(BoxCollider)) { var downCasted = (BoxCollider)component; downCasted.enabled = value; }
-                else if (type == typeof(SphereCollider)) { var downCasted = (SphereCollider)component; downCasted.enabled = value; }
-                else if (type == typeof(CapsuleCollider)) { var downCasted = (CapsuleCollider)component; downCasted.enabled = value; }
-                else if (type == typeof(MeshCollider)) { var downCasted = (MeshCollider)component; downCasted.enabled = value; }
-                else if (type == typeof(WheelCollider)) { var downCasted = (WheelCollider)component; downCasted.enabled = value; }
-                //else if (type == typeof(TerrainCollider)) { var downCasted = (TerrainCollider)component; downCasted.enabled = enabled; }
-                // Renderer
-                else if (type == typeof(MeshRenderer)) { var downCasted = (MeshRenderer)component; downCasted.enabled = value; }
-                else if (type == typeof(SkinnedMeshRenderer)) { var downCasted = (SkinnedMeshRenderer)component; downCasted.enabled = value; }
-                else if (type == typeof(LineRenderer)) { var downCasted = (LineRenderer)component; downCasted.enabled = value; }
-                else if (type == typeof(TrailRenderer)) { var downCasted = (TrailRenderer)component; downCasted.enabled = value; }
-                else if (type == typeof(BillboardRenderer)) { var downCasted = (BillboardRenderer)component; downCasted.enabled = value; }
-                else if (type == typeof(SpriteRenderer)) { var downCasted = (SpriteRenderer)component; downCasted.enabled = value; }
-                //else if (type == typeof(UnityEngine.Tilemaps.TilemapRenderer)) { var downCasted = (UnityEngine.Tilemaps.TilemapRenderer)component; downCasted.enabled = enabled; }
-                // Constraint
-                else if (type == typeof(AimConstraint)) { var downCasted = (AimConstraint)component; downCasted.enabled = value; }
-                else if (type == typeof(LookAtConstraint)) { var downCasted = (LookAtConstraint)component; downCasted.enabled = value; }
-                else if (type == typeof(ParentConstraint)) { var downCasted = (ParentConstraint)component; downCasted.enabled = value; }
-                else if (type == typeof(PositionConstraint)) { var downCasted = (PositionConstraint)component; downCasted.enabled = value; }
-                else if (type == typeof(RotationConstraint)) { var downCasted = (RotationConstraint)component; downCasted.enabled = value; }
-                else if (type == typeof(ScaleConstraint)) { var downCasted = (ScaleConstraint)component; downCasted.enabled = value; }
-                // Behaviour
-                else if (type == typeof(Light)) { var downCasted = (Light)component; downCasted.enabled = value; }
-                else if (type == typeof(Camera)) { var downCasted = (Camera)component; downCasted.enabled = value; }
-                else if (type == typeof(Animator)) { var downCasted = (Animator)component; downCasted.enabled = value; }
+                if (type == typeof(GameObject)) { continue; }
                 // Extra
                 // 用途的に、アクティブならClose・非アクティブならOpenのが都合が良さそうなので反転
                 else if (type == typeof(OcclusionPortal)) { var downCasted = (OcclusionPortal)component; downCasted.open = !value; }
                 else if (type == typeof(CanvasGroup)) { var downCasted = (CanvasGroup)component; downCasted.interactable = value; }
+                // Common
+                else if (type.IsSubclassOf(typeof(Collider))) { var downCasted = (Collider)component; downCasted.enabled = value; }
+                else if (type.IsSubclassOf(typeof(Renderer))) { var downCasted = (Renderer)component; downCasted.enabled = value; }
+                // Constraint
+                else if (type == typeof(ParentConstraint)) { var downCasted = (ParentConstraint)component; downCasted.enabled = value; }
+                else if (type == typeof(PositionConstraint)) { var downCasted = (PositionConstraint)component; downCasted.enabled = value; }
+                else if (type == typeof(RotationConstraint)) { var downCasted = (RotationConstraint)component; downCasted.enabled = value; }
+                else if (type == typeof(ScaleConstraint)) { var downCasted = (ScaleConstraint)component; downCasted.enabled = value; }
+                else if (type == typeof(AimConstraint)) { var downCasted = (AimConstraint)component; downCasted.enabled = !value; }
+                else if (type == typeof(LookAtConstraint)) { var downCasted = (LookAtConstraint)component; downCasted.enabled = value; }
+                // Audio
+                else if (type == typeof(AudioSource)) { var downCasted = (AudioSource)component; downCasted.enabled = value; }
+                else if (type == typeof(AudioLowPassFilter)) { var downCasted = (AudioLowPassFilter)component; downCasted.enabled = value; }
+                else if (type == typeof(AudioHighPassFilter)) { var downCasted = (AudioHighPassFilter)component; downCasted.enabled = value; }
+                else if (type == typeof(AudioEchoFilter)) { var downCasted = (AudioEchoFilter)component; downCasted.enabled = value; }
+                else if (type == typeof(AudioDistortionFilter)) { var downCasted = (AudioDistortionFilter)component; downCasted.enabled = value; }
+                else if (type == typeof(AudioChorusFilter)) { var downCasted = (AudioChorusFilter)component; downCasted.enabled = value; }
+                else if (type == typeof(AudioReverbFilter)) { var downCasted = (AudioReverbFilter)component; downCasted.enabled = value; }
+                else if (type == typeof(AudioReverbZone)) { var downCasted = (AudioReverbZone)component; downCasted.enabled = value; }
+                // Behaviour
+                else if (type == typeof(Light)) { var downCasted = (Light)component; downCasted.enabled = value; }
+                else if (type == typeof(Camera)) { var downCasted = (Camera)component; downCasted.enabled = value; }
+                else if (type == typeof(Animator)) { var downCasted = (Animator)component; downCasted.enabled = value; }
+                // VRChat
+                else if (type == typeof(UdonBehaviour)) { var downCasted = (UdonBehaviour)component; downCasted.enabled = value; }
+                else if (type == typeof(VRCParentConstraint)) { var downCasted = (VRCParentConstraint)component; downCasted.enabled = value; }
+                else if (type == typeof(VRCPositionConstraint)) { var downCasted = (VRCPositionConstraint)component; downCasted.enabled = value; }
+                else if (type == typeof(VRCRotationConstraint)) { var downCasted = (VRCRotationConstraint)component; downCasted.enabled = value; }
+                else if (type == typeof(VRCScaleConstraint)) { var downCasted = (VRCScaleConstraint)component; downCasted.enabled = value; }
+                else if (type == typeof(VRCAimConstraint)) { var downCasted = (VRCAimConstraint)component; downCasted.enabled = value; }
+                else if (type == typeof(VRCLookAtConstraint)) { var downCasted = (VRCLookAtConstraint)component; downCasted.enabled = value; }
+                else if (type == typeof(VRCPhysBone)) { var downCasted = (VRCPhysBone)component; downCasted.enabled = value; }
+                else if (type == typeof(VRCContactSender)) { var downCasted = (VRCContactSender)component; downCasted.enabled = value; }
+                else if (type == typeof(VRCContactReceiver)) { var downCasted = (VRCContactReceiver)component; downCasted.enabled = value; }
+                // enabled が not exposed
+                // else if (type.IsSubclassOf(typeof(Behaviour))) { var downCasted = (Behaviour)component; downCasted.enabled = value; }
+                // else if (type == typeof(AudioListener)) { var downCasted = (AudioListener)component; downCasted.enabled = value; }
+                // else if (type == typeof(Cloth)) { var downCasted = (Cloth)component; downCasted.enabled = value; }
             }
         }
     }
